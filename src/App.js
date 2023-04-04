3231c1de5a45351498bae6cb4c3d2528c1ea3c0d
bayu <bayusetiawanipb@gmail.com>
2023-04-04 12:23:08 +0700

    debug

--- src/App.js
+++ src/App.js
@@ -11,12 +11,14 @@ import Detail from './Pages/Detail';
 function App() {
   return (
     <Switch>
-      <Route path="/:id" component={Detail}></Route>
       <Route path="/movies/:id" component={FormMovie}></Route>
       <Route path="/movies" component={Movies}></Route>
       <Route path="/customers" component={Customers}></Route>
       <Route path="/buy" component={Buy}></Route>
+
+      <Route path="/:id" component={Detail}></Route>
       <Route path="/" component={Homepage}></Route>
+      
       <Route path="/not-found" component={notFound}></Route>
       <Router path="/payment" component={Buy}></Router>
       <Redirect to="not-found" />
